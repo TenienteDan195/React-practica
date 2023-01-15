e955093d240f5a896ce5fcdd0313603b7bd14f80
TenienteDan195 <luislopeojeda@gmail.com>
2023-01-14 20:30:37 -0600

    hola

--- my-app/src/componentes/itemListcontainer/itemListcontainer.js
+++ my-app/src/componentes/itemListcontainer/itemListcontainer.js
@@ -1,11 +1,14 @@
 
 import React, {useEffect, useState} from "react";
+import Counter from "../counter/counter";
 import "./itemListContainer.css";
+import { products } from "../../productsMock";
 
 const ItemListcontainer =( {greeting}) => {
     //Flag o Bandera
     const [ isDark , setIsDark ] = useState(false)
     const [ num , setNum] = useState(0)
+    const [ items , setItems] = useState([])//se recomienda siempre usar el tipo de dato con el que vamos a empezar
 
     const toogleMode = () =>{
         
@@ -28,13 +31,34 @@ const ItemListcontainer =( {greeting}) => {
 
     console.log(isDark)
 
-  return (
+    const onAdd = () => {
+      console.log("hola")
+    }
+//Es una promesa con las palabras reservadas new , Promise
+//es una tarea asincrona
+    const task = new Promise ((resolve , reject)=>{
+      resolve(products)
+    })//nunca desde front-end vendran las promesas
+
+    console.log ("Esta es la promesa", task)
+
+    task
+      //set items me va a setear mi estado con eso que venga en la respuesta
+      .then((res)=>{setItems( res )})
+      .catch((err)=>{console.log("se rechazo")})
+    //se aplica a una promesa para capturar lo que la promesa resolvio
+    // se aplica para obtener lo que una promesa me lo devolvio como error
+
+    console.log(items)
+
+    return (
     //Para hacer un un modo oscuro podemos reducir linreas de codigo de la siguiente manera
     <div className= {isDark ? "container-items-dark" : "container-items-ligth"}>
     <h2>{greeting}</h2> 
     <button onClick={toogleMode}>{isDark ? "cambiar a modo claro" : "cambiar a modo oscuro"}</button>
 
     <button onClick={()=>setNum(num+1)}>sumar</button>
+    <Counter stock = {10} initial = {1} onAdd = {onAdd}/>
     </div>
   )
 }
---
+++ my-app/src/productsMock.js
@@ -0,0 +1,41 @@
+export const products = [
+    {
+        name: "nike",
+        price: 2500,
+        stock: 3,
+        description: "Deportivo",
+    },
+    {
+        name: "addias",
+        price: 3000,
+        stock: 8,
+        description: "deportivo"
+    },
+    {
+        name: "puma",
+        price: 2500,
+        stock: 3,
+        description: "Deportivo",
+    },
+    {
+        name: "panam",
+        price: 3000,
+        stock: 8,
+        description: "urbano"
+    },{
+        name: "vans",
+        price: 2500,
+        stock: 3,
+        description: "urbano",
+    },
+    {
+        name: "converse",
+        price: 3000,
+        stock: 8,
+        description: "urbano"
+    },
+    {
+
+    }
+
+]
